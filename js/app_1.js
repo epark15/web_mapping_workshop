da0830786340b9394532a7d5345904d20c17c93c
epark15 <epark15@gwmail.gwu.edu>
2015-03-31 20:30:53 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -11,4 +11,19 @@ var map = L.mapbox.map('map', mapId);
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
 
+var datafileToAdd = 'data/parks.geojson';
+
+var featurelayer = l.mapbox.featurelayer().loadUrl(datafileToAdd).addTo(map);
+
+featurelayer.on('ready',function(){
+    this.setStyle({
+      'color':'#ec008c',
+      'fillColor':'#ec008c',
+      'weight':4,
+      'opacity':.7
+    })
+    map.fitBounds(featurelayer.getBounds());
+})
+  
+
 
